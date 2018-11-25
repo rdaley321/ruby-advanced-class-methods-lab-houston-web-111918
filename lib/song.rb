668b236acbe15debe089fe2b1676a1946d6007ee
Ryan Daley <rdaley321@yahoo.com>
2018-11-25 05:41:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,7 @@ class Song
   def self.new_from_filename(filename)
     new = self.new
     array = filename.split(/\-/)
-    new.name = array.first
+    new.name = array.first.split(".mp3")
     new.artist_name = array[1]
   end
 end
