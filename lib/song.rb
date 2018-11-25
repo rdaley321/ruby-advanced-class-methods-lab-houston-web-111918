bc48eb7adac397a5f8be871d268cf60818997ba0
Ryan Daley <rdaley321@yahoo.com>
2018-11-25 05:32:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,7 @@ class Song
   def self.new_from_filename(filename)
     new = self.new
     array = filename.split(/\-/)
-    array[1].split!(".mp3")
+    array[1].split(".mp3")
     puts array
   end
 end
